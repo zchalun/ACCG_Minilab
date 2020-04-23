c01cc874e5802fa25c6bdfff9b7abb831d92ac4a
azhan <azhan@192.168.86.51>
2020-04-23 15:05:22 -0700

    Ahahaha Here it is the rewrote CircleQueueAL Class

--- APSelectionLab/APSelectionLab/CircleQueueAL.java
+++ APSelectionLab/APSelectionLab/CircleQueueAL.java
@@ -5,7 +5,9 @@
  */
 import java.util.*;
 public class CircleQueueAL {
-	ArrayList<LinkedList> ob = new ArrayList<LinkedList>();
+	ArrayList<Object> ob = new ArrayList<Object>();
+	private int[] slist; int scompares = 0; int sswaps = 0;
+	Object opaqueObject;
 	/**
 	 * get the first object in the arrayList
 	 */
@@ -13,7 +15,7 @@ public class CircleQueueAL {
 		if(ob.get(0) == null) {
 			return null;
 		}else {
-			return ob.get(0).getObject();
+			return ob.get(0);
 		}
 		
 	}
@@ -24,13 +26,69 @@ public class CircleQueueAL {
 		if(ob.get(ob.size()-1) == null) {
 			return null;
 		}else {
-			return ob.get(ob.size()-1).getObject();
+			return ob.get(ob.size()-1);
 		}
+	}
+
+	/**
+	 * add stuff to the list
+	 */
+	public void add(Object opaqueObject) {
+		ob.add(opaqueObject);
+	}
+	/**
+	 * Delete an object from the front of the Queue,
+	 */
+	public Object delete() {
+		 Object opaqueObject = null;
+	  	  
+		  if (ob.get(0)!= null) {
+			  opaqueObject = ob.get(0); 		  				 
+		  }	 
+		  ob.remove(0);
+		  return opaqueObject;
 	}
 	/**
 	 * 
 	 */
+	 public String toString()
+	  {
+	    String queueToString = "[";
+	    for(int i = 0; i<ob.size();i++) {
+	    	if(ob.get(i)!=null) {
+	    	queueToString += "("+ob.get(i)+")";
+	    		if(ob.get(i+1)!=null) {
+	    			queueToString += ", ";
+	    		}
+	    	}
+	    }
+	    queueToString += "]";
+	    return queueToString;
+
+	    
+	  }
+	  private int[] SelectionSort() {
+	    	for (int i = 0; i < slist.length - 1; i++)
+	    	{
+	    		//find min val in unsorted part of array
+	    		int minIndex = i;
+	    		for (int j=i+1; j < slist.length; j++)
+	    			if (slist[j] < slist[minIndex])
+	    			minIndex = j;    				
+	    			
+	    			// swap min element with current first index
+	    			int temp = slist[minIndex];
+	    			slist[minIndex] = slist[i];
+	    			slist[i] = temp;
+	    			
+	    			//stats
+	    			this.scompares++;
+					this.sswaps++;
+	    			}
+	    	return slist;
+	    	}
 }
+
 	
 		
 	
